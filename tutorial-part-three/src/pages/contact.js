be6c83a9ab417931d7f6ca65d24dc8b82d609bc9
hecpalomares <hpalomares12@gmail.com>
2018-04-09 23:48:05 -0500

    Tutorial Part Three: Building Nested Layouts

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>I'd love to chat! Email me at the address below</h1>
+    <p>
+      <a href="mailto:hector.palomares92@gmail.com">hector.palomares92@gmail.com</a>
+    </p>
+  </div>
+);
