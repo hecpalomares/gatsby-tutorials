14996f18915a654d58bb854af0d62422a5288e19
hecpalomares <hpalomares12@gmail.com>
2018-04-14 23:47:19 -0500

    Chapter 6: Transformer plugins

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -1,13 +1,40 @@
-import React from "react"
+import React from "react";
+import g from "glamorous";
 
-export default () => (
+import { rhythm } from "../utils/typography";
+
+export default ({data}) => (
   <div>
-    <h1>Amazing Pandas Eating Things</h1>
-    <div>
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="Group of pandas eating bamboo"
-      />
-    </div>
+    <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
+      Amazing Pandas Eating Things
+    </g.H1>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <g.H3 marginBottom={rhythm(1 / 4)}>
+          {node.frontmatter.title}{" "}
+          <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+        </g.H3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </div>
 );
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
